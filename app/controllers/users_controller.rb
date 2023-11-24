5006710ca350acac0d423a701eb4015f1862a869
Kasper <kasperbak89@pm.me>
2023-11-24 12:03:33 +0100

    Add view files, update routes and controllers to enable user edition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def
+
+  def show
+  end
+
+  def index
+    @users = User.all
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 end
